506220c8fea801da12a589ea26c8889f5f64fa15
ender18g <allan.elsberry@gmail.com>
2022-01-01 15:39:39 -0500

    turned esp32 code to class

---
+++ esp32_upython/Camboard.py
@@ -0,0 +1,135 @@
+import urequests
+from machine import reset, Pin
+import time
+from display import white, black, white_box
+from takePhoto import capture_post
+import gc
+import network
+
+class Camboard:
+  #must provide a url with the settings object
+  def __init__(self,url,ssid,password):
+    #these are not actual settings
+    self.last_set = time.time()
+    self.on_time = time.time()
+    self.url = url
+    self.last_shot = time.time()
+    self.move_count = 0
+    self.dots =0
+    # connect to wifi
+    self.do_connect(ssid,password)
+    self.init_pins()
+    self.pull_settings()
+    #enable the garbage collector
+    gc.enable()
+
+  def pull_settings(self):
+    print('pulling settings')
+    data = {}
+    #reset the pull settings time
+    self.last_set=time.time()
+    #now try to pull all of our settings and save them
+    try:
+      data = urequests.get(self.url).json()
+      print(data)
+    except:
+      print('Cannot Pull Data')
+      reset()
+      return None
+    self.armed = bool(data.get('armed',True))
+    self.quality = int(data.get('quality',40))
+    self.delay = int(data.get('delay',2))
+    self.pre_delay = int(data.get('pre_delay',0))
+    self.min_move = int(data.get('min_move',5))
+    self.update_period = int(data.get('update_period',60*5))
+    self.max_on_time = int(data.get('max_on_time',60*15))
+
+  def check_update(self):
+    if time.time()-self.last_set>self.update_period:
+      self.last_set = time.time()
+      self.pull_settings()
+  
+  def check_reset(self):
+    if time.time()-self.on_time>self.max_on_time:
+      black(' RESTARTING')
+      print('Scheduled restart')
+      reset()
+  
+  def check_arm_btn(self):
+    #if our button is pressed
+    if not self.btn.value():
+      #toggle the armed value
+      self.armed= not self.armed
+      if self.armed:
+        white('  --ARMED--')
+      else:
+        black('  Disarmed')
+      time.sleep(1)
+
+  def check_all(self):
+    self.check_reset()
+    self.check_update()
+    self.check_arm_btn()
+
+  def do_connect(self,ssid,password):
+    k=0
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(ssid,password)
+        while not wlan.isconnected():
+            k+=1
+            dots = '.'*(k % 4)
+            black('  Connecting' +  dots)
+            pass
+    print('network config:', wlan.ifconfig())
+    #sleep for 2 seconds after connecting
+    time.sleep(2)
+
+  def init_pins(self):
+    #init the PIR pin
+    self.pir = Pin(33, Pin.IN)
+    # btn is 0 when pressed
+    self.btn = Pin(34, Pin.IN)
+
+  def show_monitor(self):
+    #reset move count
+    self.move_count=0
+    if self.armed:
+      black(' *Monitoring*' + self.dots*'.')
+    else:
+      black(' Monitoring' + self.dots*'.')
+    self.dots=0 if self.dots>2 else self.dots+1
+
+  def check_motion(self):
+    # if motion detected
+    if self.pir.value():
+      white('  Motion: ' + str(self.move_count))
+      #incr move count
+      self.move_count+=1
+      if self.armed:
+        now = time.time()
+        if now-self.last_shot>self.delay and self.move_count>self.min_move:
+          #sleep for pre-delay
+          time.sleep_ms(self.pre_delay)
+          #show a white box on the screen
+          white_box()
+          print(gc.mem_free())
+          #take photo
+          print('Taking Photo')
+          capture_post(self.quality)
+          #clear unused memory!
+          gc.collect()
+          #record the last shot time
+          self.last_shot = now
+    else:
+      self.show_monitor()
+
+  def monitor(self):
+    while True:
+      #check all update conditions
+      self.check_all()
+      self.check_motion()
+      time.sleep_ms(100)
+
--- esp32_upython/main.py
+++ esp32_upython/main.py
@@ -1,125 +1,15 @@
-#import wifimgr
-from display import white, black, white_box
-from machine import Pin, reset
-import time
-from takePhoto import capture_post
-import urequests
-import gc
-gc.enable()
-print(gc.mem_free())
-i = 0
+from Camboard import Camboard
+from machine import reset
+
+
+# init camboard
+url = 'http://allan18g.pythonanywhere.com'
+camboard = Camboard(url,'SUAS','usna-suas')
 
 
 
-# white('Wifi Setup...')
-# wlan = wifimgr.get_connection()
-# if wlan is None:
-#     print("Could not initialize the network connection.")
-#     while True:
-#         pass  # you shall not pass :D
-
-#Connect to our WIFI
-
-def do_connect():
-    import network
-    k=0
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    if not wlan.isconnected():
-        print('connecting to network...')
-        wlan.connect('SUAS', 'usna-suas')
-        while not wlan.isconnected():
-            k+=1
-            dots = '.'*(k % 4)
-<<<<<<< HEAD
-            black('Connecting' +  dots)
-=======
-            black('  Connecting' +  dots)
->>>>>>> 50bc0a380ba744376999b7030e4fc0b3e9cb56f2
-            pass
-    print('network config:', wlan.ifconfig())
-
-do_connect()
-
-
-
-# Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
-print("ESP OK")
-black('  Connected')
-time.sleep(2)
-
-# get the delay and quality values
-settings_url = 'http://allan18g.pythonanywhere.com/'
-settings = urequests.get(settings_url).json()
-print(settings)
-
-armed = settings.get('armed', True)
-quality = int(settings.get('quality', 30))
-delay = int(settings.get('delay', 5))
-pre_delay = int(settings.get('pre_delay', 0))
-min_move = int(settings.get('min_move', 4))
-
-
-pir = Pin(33, Pin.IN)
-# btn is 0 when pressed
-btn = Pin(34, Pin.IN)
-# record seconds so that we can avoid too manys photos
-last_shot = time.time()
-
-# we will restart every x seconds
-max_on_time = 60*60
-on_time = time.time()
-
-#keep track of persistent movement
-move_count=0
-
-# num of dots to show
-dots=0
-
-black(' Monitoring...')
-
-
-while True:
-    if time.time()-on_time > max_on_time:
-        reset()
-
-    if not btn.value():
-        # if btn is pressed alternate arming the camera
-        armed = not armed
-        if armed:
-            white('  --ARMED--')
-        else:
-            black('  Disarmed')
-        time.sleep(2)
-
-    # if motion detected, edit screen
-    if pir.value():
-        white('  Motion: ' + str(move_count))
-        #increment our move counter
-        move_count +=1
-        #record the time so that we can check on the delay
-        now = time.time()
-        #if it's beyond the delay, armed, and meets # movements take photo
-        if now-last_shot > delay and (armed and move_count>=min_move):
-            #sleep for pre-delay
-            time.sleep_ms(pre_delay)
-            #show a white box on the screen
-            white_box()
-            print(gc.mem_free())
-            #take photo
-            capture_post(quality)
-            #clear unused memory!
-            gc.collect()
-            #record the last shot time
-            last_shot = now
-    else:
-        #no motion was detected
-        move_count=0
-        #now implement the monitoring... routine
-        if armed:
-            black(' *Monitoring*' + dots*'.')
-        else:
-            black(' Monitoring' + dots*'.')
-        dots=0 if dots>2 else dots+1
-
-    time.sleep_ms(100)
+#run camboard in try/except
+try:
+    camboard.monitor()
+except:
+    reset()
